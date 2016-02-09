37bd319aed7ca5352da0bed47314989d4f78867c
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2016-02-09 15:16:27 +0100

    Create keepUptoDate_sourceCode.bat

---
+++ synchronize/keepUptoDate_sourceCode.bat
@@ -0,0 +1,35 @@
+@echo off
+
+:: assumes that local computer runs with windows and remote with linux
+::
+:: set the following parameters previsouly:
+:: localComputer, set remoteComputer, code, branch, login, pwd, hostName
+:: a file winscp_keepUpToDate.txt will be generated in temporary folder - you can delete that then
+
+echo.
+echo __()_()________________________________________________________________________
+echo __('.')__________________________ KEEPING UP TO DATE __________________________
+echo __()  icbc 0.2 __________________ %code% %branch%      _________
+echo _________________________________ ON %remoteComputer% ___________________________
+echo.
+:::::::::::::::
+
+set WINSCP=C:\"Program Files (x86)"\WinSCP\WinSCP.com
+set tempFolder=C:\Windows\Temp
+
+set winscpScript=%tempFolder%\winscp_keepUpToDate_%code%_%branch%_%remoteComputer%.txt
+
+
+echo option batch abort > %winscpScript%
+echo option confirm off >> %winscpScript%
+echo open sftp://%login%:%pwd%@%hostName%/ >> %winscpScript%
+echo keepuptodate %localRoot%\%localComputer%\%code%\%branch%\sources %remoteRoot%/%remoteComputer%/%code%/%branch%/sources >> %winscpScript%
+
+
+call %WINSCP% /script=%winscpScript%
+
+: del F:\tools\winscp\winscp_keepUpToDate.txt
+: removed since it does not delete
+
+: pause
+
